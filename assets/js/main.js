14ba1d78af6c951449db80bfb27c648c922c4edc
wass01 <maatallawassim01@gmail.com>
2021-04-09 11:52:30 +0200

    creazione file

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Attraverso una chiamata ajax all’API di boolean https:
+//flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+Vue.config.devtools = true;
+
+let app = new Vue({
+  el: "#root",
+  data: {
+  },
+
+  methods: {
+  }
+});
